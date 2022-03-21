250768adb3b084fe84c960a1831a9483a336ff1f
Jessica Kincaid <4j.kincaid@gmail.com>
2022-03-21 15:47:20 -0500

    create first wagtail site

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-g-ns+ume0n9x-xcdz7oxegt@$%5_mj0_f=)4+_5-x6aj4q^o=!'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
